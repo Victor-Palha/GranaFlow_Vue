efd64a1b1540bf4bb87fc94b264696d6f87ca59d
strahd <jv.palha@gmail.com>
2025-03-31 17:44:55 -0300

    merge: switch to prod

--- src/constants/URLS.ts
+++ src/constants/URLS.ts
@@ -1,5 +1,5 @@
 export const URLS = {
     // api: "http://10.0.1.93:4000"
-    api: "http://localhost:4000"
-    // api: "https://grana-flow.fly.dev"
+    // api: "http://localhost:4000"
+    api: "https://grana-flow.fly.dev"
 }
